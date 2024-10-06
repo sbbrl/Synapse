17c90086d1df784735cd241fec8c059c2f58ca49
sbbrl <git@subibhu.info.np>
2024-10-06 15:32:49 +0000

    Favicon , Fixes

--- synapse/synapse/urls.py
+++ synapse/synapse/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from webapp import views
+from webapp import views, urls
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('webapp/',include('webapp.urls')),
+    path('',include('webapp.urls')),
 ]
